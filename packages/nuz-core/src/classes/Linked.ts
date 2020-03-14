d77eb8625e174461fb80942dbf3b1d9210a4d455
Hieu Lam <lamhieu.vk@gmail.com>
2020-03-14 21:23:24 +0700

    feat(nuz-core): support watching mode to hot reload modules

--- packages/nuz-core/src/classes/Linked.ts
+++ packages/nuz-core/src/classes/Linked.ts
@@ -1,4 +1,5 @@
-import { linkedUrls } from '@nuz/utils'
+import { checkIsUrlOk, constants, linkedUrls } from '@nuz/utils'
+import socket from 'socket.io-client'
 
 import { BootstrapConfig } from '../types'
 
@@ -6,17 +7,52 @@ import fetchWithTimeout from '../utils/fetchWithTimeout'
 
 class Linked {
   private readonly url: string
+  private readonly io: SocketIOClient.Socket
+  private readonly watching: string[]
 
   constructor(private readonly linked: BootstrapConfig['linked']) {
     if (!this.linked) {
       return
     }
 
-    this.url = linkedUrls.modules(this.linked.port)
+    const { port } = this.linked
+
+    // Set url for linked module
+    this.url = linkedUrls.modules(port)
+
+    // Create connection and save io and watch helper
+    const watch = linkedUrls.watch(port)
+    this.io = socket(watch.host, {
+      path: watch.path,
+    })
+
+    // Create empty watching list
+    this.watching = []
+
+    // Listen change event from server
+    console.log(constants.CHANGE_EVENT, 'constants.CHANGE_EVENT')
+    this.io.on(constants.CHANGE_EVENT, ({ modules }) => {
+      const isChanged = modules.some(module => this.watching.includes(module))
+      console.log({ modules, isChanged })
+      if (isChanged) {
+        window.location.reload()
+      }
+    })
+  }
+
+  private getUrl(name: string) {
+    return `${this.url}/${name}/index.js`
+  }
+
+  async isOk(name: string) {
+    const url = this.getUrl(name)
+
+    const isOk = await checkIsUrlOk(url)
+    return isOk
   }
 
   async getScript(name: string) {
-    const url = `${this.url}/${name}/index.js`
+    const url = this.getUrl(name)
 
     const response = await fetchWithTimeout(url)
     if (!response.ok) {
@@ -25,6 +61,10 @@ class Linked {
 
     const content = await response.text()
     return content
+  }
+
+  watch(modules: string[]) {
+    this.watching.push(...modules)
   }
 }
 
