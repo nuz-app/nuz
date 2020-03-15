141b64544d8d379b2e93c522447349c3f4f63b5d
Hieu Lam <lamhieu.vk@gmail.com>
2020-03-15 17:19:10 +0700

    chore(nuz-core): set version in root

--- packages/nuz-core/src/autoload.ts
+++ packages/nuz-core/src/autoload.ts
@@ -1,10 +1,11 @@
 import { ROOT_KEY } from '@nuz/shared'
 
+import { VERSION } from './env'
 import * as selfHelpers from './utils/selfHelpers'
 
 // tslint:disable-next-line: prettier
 (function autoload() {
   if (!selfHelpers.has(ROOT_KEY)) {
-    selfHelpers.set(ROOT_KEY, { version: null })
+    selfHelpers.set(ROOT_KEY, { version: VERSION })
   }
 })()
--- packages/nuz-core/src/index.ts
+++ packages/nuz-core/src/index.ts
@@ -7,4 +7,5 @@ export * from './types/common'
 export { default as load } from './load'
 export { default as resolve } from './resolve'
 export { default as bootstrap } from './bootstrap'
+
 export { default as events } from './events'
