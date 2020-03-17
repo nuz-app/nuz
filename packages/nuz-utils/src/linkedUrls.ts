790f1811ee38927fc3410f2c41100df8b432fdc3
Hieu Lam <lamhieu.vk@gmail.com>
2020-03-17 13:01:04 +0700

    chore(nuz-utils): tiny update for linked urls

--- packages/nuz-utils/src/linkedUrls.ts
+++ packages/nuz-utils/src/linkedUrls.ts
@@ -5,17 +5,17 @@ const LOCAL_HOST = 'localhost'
 export const ipv4 = () => internalIp.v4.sync() || LOCAL_HOST
 
 export const modules = (port: number): URL => {
-  const protocol = 'http:'
+  const protocol = 'http'
   const hostname = ipv4()
   const pathname = ''
 
-  return new URL(`${protocol}//${hostname}:${port}${pathname}`)
+  return new URL(`${protocol}://${hostname}:${port}${pathname}`)
 }
 
 export const watch = (port: number): URL => {
-  const protocol = 'http:'
+  const protocol = 'http'
   const hostname = ipv4()
   const pathname = '/watching'
 
-  return new URL(`${protocol}//${hostname}:${port}${pathname}`)
+  return new URL(`${protocol}://${hostname}:${port}${pathname}`)
 }
