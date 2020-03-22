4f5a900407d47b35a334d7919e031e2d5e1acf34
Hieu Lam <lamhieu.vk@gmail.com>
2020-03-22 17:15:33 +0700

    feat(nuz-cli): supports cors, https, compression for serve helpers

--- packages/nuz-cli/src/utils/serve.ts
+++ packages/nuz-cli/src/utils/serve.ts
@@ -1,19 +1,62 @@
+import { loadCertificateDefault } from '@nuz/utils'
+import compression from 'compression'
+import cors from 'cors'
+import express from 'express'
 import http from 'http'
 import handler from 'serve-handler'
+import spdy from 'spdy'
 
-const serve = ({ port, dir }: { port: number; dir: string }): http.Server => {
-  const server = http.createServer((request, response) => {
-    response.setHeader('Access-Control-Allow-Origin', '*')
-    response.setHeader('Access-Control-Request-Method', '*')
-    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
-    response.setHeader('Access-Control-Allow-Headers', '*')
+import checkIsProductionMode from './checkIsProductionMode'
 
-    return handler(request, response, {
+export interface ServeConfig {
+  port: number
+  dir: string
+  https?: boolean | any
+  compression?: boolean | any
+  cors?: boolean | any
+}
+
+const defaultConfig = {
+  cors: checkIsProductionMode() ? false : true,
+  compression: checkIsProductionMode() ? false : true,
+}
+
+const serve = (config: ServeConfig) => {
+  const {
+    port,
+    dir,
+    https,
+    compression: compress,
+    cors: corsc,
+  } = Object.assign({}, defaultConfig, config)
+
+  let server
+
+  const app = express()
+
+  if (compress) {
+    app.use(compression(compress === true ? {} : compress))
+  }
+
+  if (corsc) {
+    app.use(cors(corsc === true ? {} : corsc))
+  }
+
+  app.use((request, response) =>
+    handler(request, response, {
       public: dir,
       symlinks: true,
       etag: true,
-    })
-  })
+    }),
+  )
+
+  if (https) {
+    const httpsConfig =
+      https === true ? Object.assign({}, loadCertificateDefault()) : https
+    server = spdy.createServer(httpsConfig, app)
+  } else {
+    server = http.createServer(app)
+  }
 
   return server.listen(port)
 }
