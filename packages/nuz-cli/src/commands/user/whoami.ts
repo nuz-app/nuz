a81f25f3ca8c1195bc22cde893cde8f1693d8b15
Hieu Lam <lamhieu.vk@gmail.com>
2020-04-24 13:14:08 +0700

    fix(nuz-cli): fix spelling `whoami`

--- packages/nuz-cli/src/commands/user/whoami.ts
+++ packages/nuz-cli/src/commands/user/whoami.ts
@@ -3,15 +3,15 @@ import Config from '../../classes/Config'
 import { NUZ_DEFAULT_USERNAME } from '../../lib/const'
 import print, { info } from '../../utils/print'
 
-async function whoiam() {
-  const who = await Config.whoiam()
-  if (who.username === NUZ_DEFAULT_USERNAME) {
+async function whoami() {
+  const { username } = await Config.whoami()
+  if (username === NUZ_DEFAULT_USERNAME) {
     info('You are using the default configuration!')
     return true
   }
 
-  info(`You are ${print.name(who.username)}.`)
+  info(`You are ${print.name(username)}.`)
   return true
 }
 
-export default whoiam
+export default whoami
