c1165db58f8e932a87449492832ce7c5b6dc6bae
Hieu Lam <lamhieu.vk@gmail.com>
2020-05-16 00:25:39 +0700

    fix(nuz-utils): lint of code

--- packages/nuz-utils/src/ensureOriginSlash.ts
+++ packages/nuz-utils/src/ensureOriginSlash.ts
@@ -2,6 +2,7 @@ const ensureOriginSlash = (value: string): string | undefined => {
   try {
     const url = new URL(value)
     return url.origin + '/'
+    // tslint:disable-next-line: no-empty
   } catch {}
 }
 
