5e96701234103a0e11dafec11cd2647e5965389e
Luan Nguyen <luanamateur@gmail.com>
2020-05-21 23:47:14 +0700

    test: update test cases for checkIsProductionMode

--- packages/nuz-utils/src/checkIsProductionMode.spec.ts
+++ packages/nuz-utils/src/checkIsProductionMode.spec.ts
@@ -1,5 +1,16 @@
-import checkIsProductionMode from './checkIsProductionMode'
+import * as func from './checkIsProductionMode'
 
-it('Export is a function', () => {
-  expect(checkIsProductionMode).toBeInstanceOf(Function)
+describe('checkIsProductionMode', () => {
+  it('Exported as default', () => {
+    expect(func.default).toBeDefined()
+  })
+
+  it('Export is a function', () => {
+    expect(func.default).toBeInstanceOf(Function)
+  })
+
+  it('Should return a boolean value', () => {
+    expect(typeof func.default()).toBe("boolean")
+  })
 })
+
