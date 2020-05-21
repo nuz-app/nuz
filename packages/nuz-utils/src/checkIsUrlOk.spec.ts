66347cd11715729d86ce329a5424fd9011d29017
Luan Nguyen <luanamateur@gmail.com>
2020-05-21 23:59:54 +0700

    test: update test cases for checkIsUrlOk

--- packages/nuz-utils/src/checkIsUrlOk.spec.ts
+++ packages/nuz-utils/src/checkIsUrlOk.spec.ts
@@ -1,8 +1,18 @@
+import * as func from './checkIsUrlOk'
 import checkIsUrlOk from './checkIsUrlOk'
 
 describe('checkIsUrlOk', () => {
+  it('Exported as default', () => {
+    expect(func.default).toBeDefined()
+  })
+
   it('Export as a function', () => {
     expect(checkIsUrlOk).toBeInstanceOf(Function)
+  })
+
+  it('Should return a boolean value', async () => {
+    const result = await checkIsUrlOk('fb.com');
+    expect(typeof result).toBe("boolean")
   })
 })
 
