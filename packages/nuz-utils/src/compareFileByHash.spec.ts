329ef8a86d4ce19d8524ad677cf95958f6192e57
Luan Nguyen <luanamateur@gmail.com>
2020-05-21 23:49:03 +0700

    test: write tests for compareFilesByHash

---
+++ packages/nuz-utils/src/compareFileByHash.spec.ts
@@ -0,0 +1,33 @@
+import * as func from './compareFilesByHash'
+import compareFilesByHash from './compareFilesByHash';
+
+describe('compareFilesByHash', () => {
+  it('Exported as default', () => {
+    expect(func.default).toBeDefined()
+  })
+
+  it('Export as a function', () => {
+    expect(compareFilesByHash).toBeInstanceOf(Function)
+  })
+
+  it('Should return a boolean value', () => {
+    const hashFile = jest.fn((a: string, hash: string) => 'abcxyz')
+    const fileA = require.resolve('./compareFilesByHash');
+    const fileB = require.resolve('./compareFilesByHash');
+    expect(typeof compareFilesByHash(fileA, fileB)).toBe('boolean')
+  })
+
+  it('Should return a true value', () => {
+    const hashFile = jest.fn((a: string, hash: string) => 'abcxyz')
+    const fileA = require.resolve('./compareFilesByHash');
+    const fileB = require.resolve('./compareFilesByHash');
+    expect(compareFilesByHash(fileA, fileB)).toBe(true)
+  })
+
+  it('Should return a true value', () => {
+    const hashFile = jest.fn((a: string, hash: string) => 'abcxyz')
+    const fileA = require.resolve('./compareFilesByHash');
+    const fileB = require.resolve('./deferedPromise');
+    expect(compareFilesByHash(fileA, fileB)).toBe(false)
+  })
+})
