fa361477df23bf56917fabb726f59882ecb590a2
Hieu Lam <lamhieu.vk@gmail.com>
2020-05-29 12:19:59 +0700

    fix(website): remove `@docusaurus/plugin-ideal-image` plugin

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -107,15 +107,15 @@ module.exports = {
       },
     ],
   ],
-  plugins: [
-    [
-      '@docusaurus/plugin-ideal-image',
-      {
-        quality: 85,
-        max: 1030, // max resized image's size.
-        min: 640, // min resized image's size. if original is lower, use that size.
-        steps: 2, // the max number of images generated between min and max (inclusive)
-      },
-    ],
-  ],
+  // plugins: [
+  //   [
+  //     '@docusaurus/plugin-ideal-image',
+  //     {
+  //       quality: 85,
+  //       max: 1030, // max resized image's size.
+  //       min: 640, // min resized image's size. if original is lower, use that size.
+  //       steps: 2, // the max number of images generated between min and max (inclusive)
+  //     },
+  //   ],
+  // ],
 };
