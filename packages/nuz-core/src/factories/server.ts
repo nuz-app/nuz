2ff7df6748035381d9f045f25eae2973665ba92e
lamhieu-vk <lamhieu.vk@gmail.com>
2020-08-20 17:34:40 +0700

    feat: add hooks for server factory flow

--- packages/nuz-core/src/factories/server.ts
+++ packages/nuz-core/src/factories/server.ts
@@ -4,6 +4,8 @@ import * as extractorHelpers from './react/extractorHelpers'
 import inject from './react/inject'
 import Loadable from './react/Loadable'
 
+type Hook = () => Promise<any>
+
 export async function prepare() {
   inject({
     react: require('react'),
@@ -16,18 +18,25 @@ export async function prepare() {
   })
 }
 
-export async function setup() {
+export async function setup(other?: Hook) {
   await Promise.all([
     shared.process.ready(),
     shared.extractor.setup(),
     Loadable.preloadAll(),
+    typeof other === 'function' ? other() : Promise.resolve(),
   ])
 }
 
-export async function teardown() {
+export async function teardown(other?: Hook, updater?: Hook) {
   await Promise.all([
     shared.process.closeSession(),
     shared.extractor.teardown(),
-    shared.process.checkUpdate(() => Loadable.flushAll()),
+    shared.process.checkUpdate(() =>
+      Promise.all([
+        Loadable.flushAll(),
+        typeof updater === 'function' ? updater() : Promise.resolve(),
+      ]),
+    ),
+    typeof other === 'function' ? other() : Promise.resolve(),
   ])
 }
