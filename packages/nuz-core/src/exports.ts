96eb8db34d7aadf0938163c20f7bbe14a2c590b9
lamhieu-vk <lamhieu.vk@gmail.com>
2020-08-21 09:54:43 +0700

    chore: exports shared process, extractor and state instances

--- packages/nuz-core/src/exports.ts
+++ packages/nuz-core/src/exports.ts
@@ -1,8 +1,9 @@
 export { default as load } from './load'
 export { default as require, default as import } from './require'
 
+export { process, extractor, state } from './shared'
+
 export * as server from './factories/server'
+export * as next from './factories/next'
 export * as react from './factories/react'
 export { Loadable } from './factories/react'
-
-export * as next from './factories/next'
