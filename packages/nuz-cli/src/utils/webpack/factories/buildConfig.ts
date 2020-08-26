9c222ff42137211fb5a7c157ca839d7c07682015
lamhieu-vk <lamhieu.vk@gmail.com>
2020-08-26 16:58:29 +0700

    chore(nuz-cli): update babel config

--- packages/nuz-cli/src/utils/webpack/factories/buildConfig.ts
+++ packages/nuz-cli/src/utils/webpack/factories/buildConfig.ts
@@ -345,12 +345,81 @@ function webpackConfigFactory(
     options: {
       cacheDirectory: cache ? cacheDirectories.babel : false,
       presets: [
-        resolveModule('@babel/preset-env'),
-        feature.react && resolveModule('@babel/preset-react'),
-      ].filter(Boolean) as string[],
+        [
+          resolveModule('@babel/preset-env'),
+          {
+            // Allow importing core-js in entrypoint and use browserlist to select polyfills
+            useBuiltIns: 'entry',
+            // Set the corejs version we are using to avoid warnings in console
+            corejs: 3,
+            // Do not transform modules to CJS
+            modules: false,
+            // Exclude transforms that make all code slower
+            exclude: ['transform-typeof-symbol'],
+          },
+        ],
+        feature.react && [
+          resolveModule('@babel/preset-react'),
+          {
+            // Adds component stack to warning messages
+            // Adds __self attribute to JSX which React will use for some warnings
+            development: dev,
+            // Will use the native built-in instead of trying to polyfill
+            // behavior for any plugins that require one.
+            useBuiltIns: true,
+          },
+        ],
+      ].filter(Boolean) as any[],
       plugins: [
-        resolveModule('@babel/plugin-syntax-dynamic-import'),
-        resolveModule('@babel/plugin-transform-runtime'),
+        // Enable loose mode to use assignment instead of defineProperty
+        // See discussion in https://github.com/facebook/create-react-app/issues/4263
+        [
+          resolveModule('@babel/plugin-proposal-class-properties'),
+          {
+            loose: true,
+          },
+        ],
+
+        // Adds Numeric Separators
+        [resolveModule('@babel/plugin-proposal-numeric-separator')],
+
+        // Polyfills the runtime needed for async/await, generators, and friends
+        // https://babeljs.io/docs/en/babel-plugin-transform-runtime
+        [
+          resolveModule('@babel/plugin-transform-runtime'),
+          {
+            corejs: false,
+            helpers: true,
+            // By default, babel assumes babel/runtime version 7.0.0-beta.0,
+            // explicitly resolving to match the provided helper functions.
+            // https://github.com/babel/babel/issues/10261
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            version: require('@babel/runtime/package.json').version,
+            regenerator: true,
+            // https://babeljs.io/docs/en/babel-plugin-transform-runtime#useesmodules
+            // We should turn this on once the lowest version of Node LTS
+            // supports ES Modules.
+            useESModules: true,
+            // Undocumented option that lets us encapsulate our runtime, ensuring
+            // the correct version is used
+            // https://github.com/babel/babel/blob/090c364a90fe73d36a30707fc612ce037bdbbb24/packages/babel-plugin-transform-runtime/src/index.js#L35-L42
+            absoluteRuntime: path.dirname(
+              resolveModule('@babel/runtime/package.json'),
+            ),
+          },
+        ],
+
+        // Optional chaining and nullish coalescing are supported in @babel/preset-env,
+        // but not yet supported in webpack due to support missing from acorn.
+        // These can be removed once webpack has support.
+        // See https://github.com/facebook/create-react-app/issues/8445#issuecomment-588512250
+        [resolveModule('@babel/plugin-proposal-optional-chaining')],
+        [resolveModule('@babel/plugin-proposal-nullish-coalescing-operator')],
+        // Transform React display name, use function name as display name
+        // [require.resolve('@babel/plugin-transform-react-display-name')],
+        // This plugin transforms ECMAScript modules to CommonJS.
+        [resolveModule('@babel/plugin-transform-modules-commonjs')],
+        [resolveModule('@babel/plugin-syntax-dynamic-import')],
       ],
     },
   })
