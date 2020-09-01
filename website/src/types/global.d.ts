c2748e5a873d66b750e976e81bd3dedfe5739d4b
lamhieu-vk <lamhieu.vk@gmail.com>
2020-09-01 11:20:43 +0700

    chore(website): using typescript and update deprecated config

---
+++ website/src/types/global.d.ts
@@ -0,0 +1,50 @@
+/// <reference types="node" />
+
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.module.less' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.png' {
+  const value: string
+  export default value
+}
+
+declare module '*.jpg' {
+  const value: string
+  export default value
+}
+
+declare module '*.jpeg' {
+  const value: string
+  export default value
+}
+
+declare module '*.gif' {
+  const value: string
+  export default value
+}
+
+declare module '*.txt' {
+  const value: string
+  export default value
+}
+
+declare module '*.svg' {
+  interface ISvg {
+    [key: string]: any
+    ReactComponent: any
+  }
+  const svg: ISvg
+  export default svg
+}
